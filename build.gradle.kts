032ee741e334a4278b401f8a277766aa43e64679
elschuyler <elvilewis40@gmail.com>
2026-03-29 00:29:39 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build configuration
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("kotlin-kapt") version "1.9.20" apply false
-}
-
-// Optional: Define versions in one place for consistency
-extra.apply {
-    set("roomVersion", "2.6.1")
-    set("navVersion", "2.7.6")
-    set("coroutinesVersion", "1.7.3")
 }

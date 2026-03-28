077a769561da432230b70e8a0ac50de4c3644cf8
elschuyler <elvilewis40@gmail.com>
2026-03-29 00:25:39 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,16 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // For Android-FilePicker
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 rootProject.name = "Vhatsai"
 include(":app")

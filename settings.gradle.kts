47e8d7242cca71d697f4b12368aab69567e41756
elschuyler <elvilewis40@gmail.com>
2026-03-29 00:29:07 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        // For Android-FilePicker
         maven { url = uri("https://jitpack.io") }
     }
 }

55e7a87d655fd27828abedcf6cb83808e2483d68
elschuyler <elvilewis40@gmail.com>
2026-03-29 21:51:23 +0530

    Update MainActivity.kt

--- app/src/main/java/com/elschuyler/vhatsai/MainActivity.kt
+++ app/src/main/java/com/elschuyler/vhatsai/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.elschuyler.vhatsai
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.elschuyler.vhatsai.databinding.ActivityMainBinding
-import com.elschuyler.vhatsai.fragment.ChatWebViewFragment
+import com.elschuyler.vhatsai.fragment.ChatFragment
 import com.elschuyler.vhatsai.fragment.InboxFragment
+import com.elschuyler.vhatsai.viewmodel.ChatViewModel
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-    private var currentWebViewFragment: ChatWebViewFragment? = null
+    private val chatViewModel: ChatViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,44 +27,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
     private fun loadInbox() {
-        currentWebViewFragment = null
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, InboxFragment())
-            .commit()
-
+        replaceFragment(InboxFragment(), "inbox")
         supportActionBar?.title = getString(R.string.app_name)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
 
-    fun openChat(url: String, name: String) {
-        currentWebViewFragment?.destroyWebView()
-
-        val fragment = ChatWebViewFragment.newInstance(url, name)
-        currentWebViewFragment = fragment
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, fragment)
-            .addToBackStack(null)
-            .commit()
-
-        supportActionBar?.title = name
+    fun openChat(chatId: Long, aiProviderId: String) {
+        val fragment = ChatFragment.newInstance(chatId, aiProviderId)
+        replaceFragment(fragment, "chat_$chatId")
+        supportActionBar?.title = aiProviderId // Use provider name or chat title
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun replaceFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment, tag)
+            .commit()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
-                currentWebViewFragment?.destroyWebView()
-                currentWebViewFragment = null
-                supportFragmentManager.popBackStack()
-                supportActionBar?.setDisplayHomeAsUpEnabled(false)
-                supportActionBar?.title = getString(R.string.app_name)
+                onBackPressed()
                 true
             }
             R.id.action_settings -> {
@@ -76,19 +62,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         if (supportFragmentManager.backStackEntryCount > 0) {
-            currentWebViewFragment?.destroyWebView()
-            currentWebViewFragment = null
             supportFragmentManager.popBackStack()
             supportActionBar?.setDisplayHomeAsUpEnabled(false)
             supportActionBar?.title = getString(R.string.app_name)
         } else {
             super.onBackPressed()
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        currentWebViewFragment?.destroyWebView()
-        currentWebViewFragment = null
     }
 }

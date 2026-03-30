28c1797aeea60beae164e2c11307dece7712361c
Schuyler Lewis <elvilewis40@gmail.com>
2026-03-31 00:30:07 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,12 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
-
 rootProject.name = "Vhatsai"
 include(":app")
